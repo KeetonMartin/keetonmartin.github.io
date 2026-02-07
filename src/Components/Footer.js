3f7c4227b5725ef056c86e8d36862fa2fe425adf
Keeton Martin <keeton.martin@gmail.com>
2026-02-07 17:32:07 -0500

    Remove template footer credit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,13 +23,7 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
+                <li>&copy; {new Date().getFullYear()} Keeton Martin</li>
               </ul>
             </div>
           </Fade>
